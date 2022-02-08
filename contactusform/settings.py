9922657483bb7939629d0a72c583466cc90e2658
Ahmed Raza <ahmedraza.webnike@gmail.com>
2022-02-08 11:07:59 +0500

    make it better

--- contactusform/settings.py
+++ contactusform/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['secret_key']
+# SECRET_KEY = os.environ['secret_key']
+SECRET_KEY = "django-insecure-@guuvjlgsg+s)g9x!h%cn^thj)p95ycwu0sgf4nt+ipwu2a42$'"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
